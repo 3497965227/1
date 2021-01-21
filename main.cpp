f3134b21721997fc19c750579204f7ade006a19e
Java1ove <75599778+3497965227@users.noreply.github.com>
2021-01-21 23:36:01 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by 张壁睿 on 2021/1/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...1
+    std::cout << "Hello, World!\n";
+    return 0;
+}

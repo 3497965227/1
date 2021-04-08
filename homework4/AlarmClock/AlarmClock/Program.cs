2882cd1ecc28e6b2751266ea15dc9cf30259938f
3497965227 <zbr1607237857@126.com>
2021-04-08 23:24:45 +0800

    1

---
+++ homework4/AlarmClock/AlarmClock/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace AlarmClock
+{
+	public delegate void Tickhandler(object sender, TickEventArgs args);
+	public delegate void Alarmhandler(object sender);
+	public class TickEventArgs
+	{
+		public int Hour { get; set; }
+		public int Minute { get; set; }
+		public int Second { get; set; }
+
+	}
+	public class AlarmColok
+	{
+		private int hour;
+		private int minute;
+		private int second;
+
+        public AlarmColok()
+        {
+        }
+
+        public AlarmColok(int hour, int minute, int second)
+		{
+			this.hour = hour;
+			this.minute = minute;
+			this.second = second;
+		}
+		public event Tickhandler Tick;
+		public event Alarmhandler Alarm;
+		public void Ticking(int hour, int minute, int second)
+		{
+			Console.WriteLine($"闹钟开始滴答，目前时间是{hour}hour.{minute}minute.{second}second");
+			TickEventArgs args = new TickEventArgs()
+			{
+				Hour = hour,
+				Minute = minute,
+				Second = second
+			};
+			Tick(this, args);
+			Alarm(this);
+		}
+	}
+	public class Form {
+		public AlarmColok alarmClock = new AlarmColok();
+		
+		public Form() {
+			
+            alarmClock.Alarm += Alarm_On;
+			alarmClock.Tick += Tick_On;
+
+		}
+
+        private void Tick_On(object sender, TickEventArgs args)
+        {
+            
+			Console.WriteLine("持续滴答");
+        }
+
+        private void Alarm_On(object sender)
+        {
+            
+			Console.WriteLine("发出闹铃");
+        }
+    }
+	class Program
+    {
+		static void Main(String[] args) {
+			Form form1 = new Form();
+			form1.alarmClock.Ticking(4, 2, 30);
+		}
+
+    }
+}
---
+++ homework4/GenericApplication/Program.cs
@@ -0,0 +1,93 @@
+﻿using GenericApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication {
+
+  // 链表节点
+  public class Node<T> {
+    public Node<T> Next { get; set; }
+    public T Data { get; set; }
+
+    public Node(T t) {
+      Next = null;
+      Data = t;
+    }
+  }
+
+  //泛型链表类
+  public class GenericList<T> {
+    private Node<T> head;
+    private Node<T> tail;
+
+    public GenericList() {
+      tail = head = null;
+    }
+
+    public Node<T> Head {
+      get => head;
+    }
+
+    public void Add(T t) {
+      Node<T> n = new Node<T>(t);
+      if (tail == null) {
+        head = tail = n;
+      }else {
+        tail.Next = n;
+        tail = n;
+      }
+    }
+
+        public static void ForEach(GenericList<T> t, Action<T> action)
+
+        {
+
+            Node<T> a = t.Head;
+
+            while (a != null)
+
+            {
+
+                action(a.Data);
+
+                a = a.Next;
+
+            }
+
+        }
+
+    }
+}
+
+  class Program {
+        static void Main(string[] args)
+        {
+      // 整型List
+      GenericList<int> intlist = new GenericList<int>();
+      for (int x = 0; x < 10; x++) {
+        intlist.Add(x);
+      }
+        /*for (Node<int> node = intlist.Head; 
+              node != null; node = node.Next) {
+          Console.WriteLine(node.Data);
+        }
+        */
+        int sum = 0, max = 0, min = 0;
+
+        GenericList<int>.ForEach(intlist, m => Console.Write($"{ m}"));
+        Console.WriteLine("");
+        GenericList<int>.ForEach(intlist, m => sum += m);
+        GenericList<int>.ForEach(intlist, m => max = max < m ? m : max);
+        GenericList<int>.ForEach(intlist, m => min = min > m ? m : min);
+        Console.WriteLine($"max is{max},min is {min},sum is {sum}");
+       
+
+
+
+    }
+   
+
+  }
